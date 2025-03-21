f7de66f14b4b227295fb10aba6484e2c90c3ce58
Danielzapirtan <antoniudanielzpirtan@gmail.com>
2025-03-21 20:29:32 +0000

    Trying to start coding a C backend

---
+++ backend.c
@@ -0,0 +1,23 @@
+/*
+ * This program is not yet functiomal.
+ * It's still under development.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef void *ANYTYPE;
+
+typedef ANYTYPE WORD;
+typedef ANYTYPE PHRASE;
+typedef ANYTYPE SENTENCE;
+typedef ANYTYPE TEXT;
+typedef ANYTYPE TREESLATED;
+
+extern void treeslateWord(
+		WORD word,
+		TREESLATED tree
+		);
+/* Add here prototypes for similar functions. */
+
+
