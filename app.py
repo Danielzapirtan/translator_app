cf879a2e146332ac9285c4552a4fd8ea446166f6
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2025-03-24 06:33:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
-from googletrans import Translator
+from deep_translator import GoogleTranslator
 import gradio as gr
-
-# Initialize the translator
-translator = Translator()
 
 def translate_text(text):
     # Translate the text from English to Romanian
-    translation = translator.translate(text, src='en', dest='ro')
-    return translation.text
+    translation = GoogleTranslator(source='en', target='ro').translate(text)
+    return translation
 
 # Create a Gradio interface
 iface = gr.Interface(
     fn=translate_text,  # Function to call
-    inputs="text",       # Input type
-    outputs="text",      # Output type
+    inputs="text",      # Input type
+    outputs="text",     # Output type
     title="English to Romanian Translator",
     description="Enter text in English and get it translated to Romanian."
 )
 
 # Launch the Gradio app
-iface.launch(debug=True)
+iface.launch()
