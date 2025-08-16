465c4e12671541c629509f98f2e0fee829a00117
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2025-08-16 14:33:19 +0000

    DBT

--- prep.sh
+++ prep.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
-cp -a en.txt en.bak.txt
-sed -ie "s/[^[:print:]]//g" en.txt
+cp -a en.bak.txt en.txt
+sed -ie "s/[^[:print:]]/ /g" en.txt
+sed -ie "s/, */, /g" en.txt
 sed -ie "s/\n/ /g" en.txt
-sed -ie "s/\. /.\n/g" en.txt
-
+exit
+sed -ie "s/\. /\./g" en.txt
+sed -ie "s/ - //g" en.txt
+sed -ie "s/Dr\.\n/Dr. /g" en.txt
+sed -ie "s/PhD/PhD/g" en.txt
